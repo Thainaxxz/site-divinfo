4b251f9a24dc6b1976bbc71e879eaf8844aa9b4c
Thainaxxz <aldasilvavs1@gmail.com>
2026-03-18 13:09:03 -0300

    adicionando requirements e procfile

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "chave-secreta-teste"
@@ -94,4 +95,5 @@ def logout():
 
 if __name__ == "__main__":
     criar_banco()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
