e82fdd4852d12d81916efbbb73e9769670b7c1aa
Thainaxxz <aldasilvavs1@gmail.com>
2026-03-31 14:27:15 -0300

    corrigindo banco render

--- app.py
+++ app.py
@@ -36,6 +36,8 @@ def criar_banco():
     conexao.commit()
     conexao.close()
 
+criar_banco()
+    
 
 @app.route("/")
 def login():
@@ -161,6 +163,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    criar_banco()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
